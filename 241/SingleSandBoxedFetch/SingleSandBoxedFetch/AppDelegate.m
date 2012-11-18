d98da8994245e224f743f676ff27dd6e7d4eca3e
Orlando Ding <xiandao.airs@gmail.com>
2012-11-18 11:48:19 +0800

    llv22 - commited of initialization of UI

--- 241/SingleSandBoxedFetch/SingleSandBoxedFetch/AppDelegate.m
+++ 241/SingleSandBoxedFetch/SingleSandBoxedFetch/AppDelegate.m
@@ -8,16 +8,51 @@
 
 #import "AppDelegate.h"
 
+//TODO : download url - http://images.apple.com/ipad/overview/images/hero_slide1.png
+
+// TODO : see http://stackoverflow.com/questions/6828831/sending-const-nsstring-to-parameter-of-type-nsstring-discards-qualifier
+// for issues
+///Users/llv22/Documents/02_apple_programming/05_wwdc/devcode/wwdc2012/241/SingleSandBoxedFetch/SingleSandBoxedFetch/AppDelegate.m:21:39: Sending 'const NSString *__strong' to parameter of type 'NSString *' discards qualifiers
+NSString* const _strSourceURL = @"sourceURL";
+NSString* const _strLastFetchURL = @"LastFetchURL";
+
+@interface AppDelegate (PrivateMethods)
+
+- (void)resetDownloadPanelToStart;
+
+@end
+
 @implementation AppDelegate
+
+#pragma mark - progress panel sheet
+
+// TODO : startProgressPanelWithMessage
+- (void)startProgressPanelWithMessage:(NSString *)message indeterminate:(BOOL)indeterminate {
+    // Display a progress panel as a sheet
+    self.progressMessage = message;
+    if (indeterminate) {
+        [self.progressIndicator setIndeterminate:YES];
+    } else {
+        [self.progressIndicator setIndeterminate:NO];
+        [self.progressIndicator setDoubleValue:0.0];
+    }
+    [self.progressIndicator startAnimation:self];
+    [self.progressCancelButton setEnabled:NO];
+    [NSApp beginSheet:self.progressPanel
+       modalForWindow:self.window
+        modalDelegate:self
+       didEndSelector:nil
+          contextInfo:NULL];
+}
 
 #pragma mark - application termination
 
 - (void)applicationWillFinishLaunching:(NSNotification*)aNotification {
-    [self addObserver:self forKeyPath:@"sourceURL" options:NSKeyValueObservingOptionNew context: NULL];
+    [self addObserver:self forKeyPath:_strSourceURL options:NSKeyValueObservingOptionNew context: NULL];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    NSString *lastFetchURL = [[NSUserDefaults standardUserDefaults] stringForKey:@"LastFetchURL"];
+    NSString *lastFetchURL = [[NSUserDefaults standardUserDefaults] stringForKey:_strLastFetchURL];
     if (lastFetchURL) {
         [self.sourceURL setStringValue:lastFetchURL];
     }
@@ -28,10 +63,20 @@
 }
 
 #pragma mark - Button action of fetch download resource and cancel download 
+
+// TODO : Fetch Action
 - (IBAction)fetch:(id)sender{
-    
+    [self resetDownloadPanelToStart];
 }
 
+// TODO : reset DownloadPanel with initialization of "Downloading Message" and fetch url
+- (void)resetDownloadPanelToStart {
+    // desc - reset UI status
+    [[NSUserDefaults standardUserDefaults] setObject:[self.sourceURL stringValue] forKey:_strLastFetchURL];
+    [self startProgressPanelWithMessage:@"Downloading..." indeterminate:NO];
+}
+
+//TODO : Save Panel Sheet
 - (IBAction)cancel:(id)sender{
     
 }
